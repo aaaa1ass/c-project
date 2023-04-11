d66fa9eee25ccd1eb2b0da625d1cd39275c552a2
Administrator <119095309+sy15963@users.noreply.github.com>
2023-04-11 21:48:39 +0900

    2023/04/11
    변수 자료형

--- cproject/cproject/ccc.c
+++ cproject/cproject/ccc.c
@@ -1,5 +1,17 @@
 #include <stdio.h> // 헤더파일에 정의된 함수가 있다 stdio.h 기본 입출력 헤더파일
 void main() //<--- 프로그램의 진입접
 {//프로그램의 시작
+	//변수 메모리 공간을 확보
+	//자료형 데이터를 저장하기 위해 정해진 데이터의 형태
+	
+	//자료형 변수의 이름
+	int data;
+	// char 한 문자 1byte
+	// short 정수 2byte
+	// int 정수 4byte
+	// float 실수 4byte
+	// double 실수 8byte		
+	// long double 8~16
+	//
 	printf("우람하고 단단한 아름다운 그것");
 }//프로그램의 끝
