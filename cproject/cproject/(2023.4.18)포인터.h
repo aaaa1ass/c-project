1ecaca9d7bcdd6b007ee6bfcf53476672653d2ac
aaaa1ass <chohs42@gmail.com>
2023-04-19 21:49:18 +0900

    2023/04/19
    포인터 상수화

---
+++ cproject/cproject/(2023.4.18)포인터.h
@@ -0,0 +1,87 @@
+#pragma once
+#include <stdio.h>
+void main()
+{
+#pragma region while
+	//특정 조건이 거짓이 될 때까지 반복
+	int count = 5;
+	while (0)//조건
+	{
+		printf("게임 실행 %d\n", count);
+		count--;
+	}
+#pragma endregion
+
+#pragma region continue
+	//해당 조건문만 실행하지 않고 반복문을 이어서 실행하는 제어문
+	for (int i = 1; i <= 0; i++)
+	{
+		if (i == 3)
+		{
+			continue;
+		}
+		printf("%d ", i);
+	}
+#pragma endregion
+
+#pragma region 형 변환
+	//다른 자료형의 변수끼리 연산이 이루어질 때 변환
+
+	//암묵적 형 변환
+	//다른 자료형끼리 연산할 때 크기가 큰 자료형으로 변환
+	//char < short < int < float < double
+	int integer = 10;
+	float decimal = 2.5f;
+	// 12.5 <- 10.0(실수) + 2.5(실수)
+	float result = integer + decimal;
+	printf("result: %f\n", result);
+
+	//명시적 형 변환
+	//사용자가 직접 자료형을 변환
+	// flaot 메모리 2.0 = 5 / 2
+	// 정수끼리의 연산은 정수만 나온다
+	// 
+	int a = 5;
+	int b = 2;
+	float result1 = (float)a / b;
+	printf("result1: %f\n", result1);
+#pragma endregion
+
+#pragma region 주소 연산자
+	//변수의 주소값을 반환
+	//시작 주소값
+	int data = 69;
+	//%p: 데이터의 주소를 출력
+	//64비트 -> 포인터 8바이트
+	//32비트 -> 포인터 4바이트 
+
+
+	//변수의 주소는 실행할 때마다 바뀐다.
+	//printf("data 주소: %p\n", &data);
+#pragma endregion
+
+#pragma region 포인터
+	//메모리의 주솟값을 저장할 수 있는 변수
+	int box = 100;
+	int box1 = 999;
+	int* ptr = &box;
+	//포인터 변수도 메모리 공간이 있고 변수의 주소를 저장하면 해당 변수의 시작 주소를 가리킨다
+	printf("ptr 값: %p\n", ptr);
+	printf("box 주소값 %p\n", &box);
+	printf("ptr 주소값 %p\n", &ptr);
+	printf("box 값: %d\n", box);
+	*ptr = 600;
+	printf("box 값: %d\n", box);
+	ptr = &box1;
+	*ptr = -999;
+	printf("box 값: %d\n", box);
+	printf("box1 값: %d\n", box1);
+	//변수의 자료형과 포인터 변수의 자료형이 일치해야 한다
+	double health = 10.5;
+	int* ptr1 = &health;
+	printf("ptr1 값: %p\n", ptr1);
+	*ptr1 = 51.5;
+	printf("ptr1가 가리키는 값: %lf\n", *ptr1);
+
+#pragma endregion		
+}
--- cproject/cproject/ccc.c
+++ cproject/cproject/ccc.c
@@ -1,87 +1,80 @@
 #include <stdio.h>
 void main()
 {
-#pragma region while
-	//특정 조건이 거짓이 될 때까지 반복
-	int count = 5;
-	while (0)//조건
-	{	
-		printf("게임 실행 %d\n", count); 
-		count--;
-	}
+	//포인터 상수화 2가지
+	//상수 지시 포인터, 포인터 상수
+#pragma region 상수지시포인터
+	//포인터 변수를  상수로 선언, 가리키고 있는 주소에 있는 값을 변경할 수 없도록 설정
+	int data = 100;
+	int data1 = 20;
+	const int* ptr = &data;
+	//*ptr = 300; 역참조가 상수화되었다
+	//printf("ptr 값: %p\n", ptr);
+	ptr = &data1;
+	//printf("ptr 값: %p\n", ptr);
 #pragma endregion
 
-#pragma region continue
-	//해당 조건문만 실행하지 않고 반복문을 이어서 실행하는 제어문
-	for (int i = 1; i <= 0; i++)
+#pragma region 포인터상수
+	//상수로 선언한 포인터이므로, 해당 변수의 값을 변경할 수 없지만, 다른 주소 값을 가리킬 수 있다
+	int value1 = 100;
+	int* const ptr1 = &value1;//포인터 변수를 상수화
+	//printf("value1 값: %d\n", value1);
+	*ptr1 = 999;//역참조 가능
+	//printf("value1 값: %d\n", value1);
+	//ptr1 = &value2; 포인터 변수가 상수화되었다
+#pragma endregion
+
+#pragma region sizeof()
+	float health = 100;
+	int* pointer;
+	printf("char 크기: %d\n", sizeof(char));
+	printf("short 크기: %d\n", sizeof(short));
+	printf("int 크기: %d\n", sizeof(int));
+	printf("long 크기: %d\n", sizeof(long));
+	printf("float 크기: %d\n", sizeof(health));
+	printf("double 크기: %d\n", sizeof(double));
+	printf("long double 크기: %d\n", sizeof(long double));
+	printf("pointer 크기: %d\n", sizeof(pointer));
+	printf("\n");
+#pragma endregion
+
+#pragma region 구구단
+	//for (int i = 1; i <= 9; i++) {
+	//	for (int j = 2; j <= 9; j++) {
+	//		printf("%d x %d = %d   ", j, i, i * j);
+	//		if (i * j < 10)
+	//			printf(" ");
+	//	}
+	//	printf("\n");
+	//}
+#pragma endregion
+
+#pragma region scanf입력함수
+	//표준 입력 함수, 여러 데이터를 서식에 맞춰 입력
+	//scanf <- sdl 검사를 해제 안하면 에러
+	//scanf_s("변수의 서식", &변수)
+	int count = 0;
+	printf("입력:");
+	//scanf_s("%d",&count);// 표준 입력 함수는 입력을 수행할 때까지 다음 작업으로 넘어갈 수 없다.
+	//printf("count: %d\n", count);
+	printf("\n");
+#pragma endregion
+	//입력값에 따라 별 계단 찍기★
+	while (1)
 	{
-		if (i == 3)
+		scanf_s("%d", &count);
+
+		for (int i = 1; i <= count; i++)
 		{
-			continue;
+			for (int j = 1; j <= i; j++)
+				printf("★");
+
+			printf("\n");
 		}
-		printf("%d ", i);
+
+		if (!count)
+			break;
 	}
-#pragma endregion
 
-#pragma region 형 변환
-	//다른 자료형의 변수끼리 연산이 이루어질 때 변환
-	
-	//암묵적 형 변환
-	//다른 자료형끼리 연산할 때 크기가 큰 자료형으로 변환
-	//char < short < int < float < double
-	int integer = 10;
-	float decimal = 2.5f;
-	// 12.5 <- 10.0(실수) + 2.5(실수)
-	float result = integer + decimal;
-	printf("result: %f\n", result);
-
-	//명시적 형 변환
-	//사용자가 직접 자료형을 변환
-	// flaot 메모리 2.0 = 5 / 2
-	// 정수끼리의 연산은 정수만 나온다
-	// 
-	int a = 5;
-	int b = 2;
-	float result1 = (float)a/b;
-	printf("result1: %f\n", result1);
-#pragma endregion
-
-#pragma region 주소 연산자
-	//변수의 주소값을 반환
-	//시작 주소값
-	int data = 69;
-	//%p: 데이터의 주소를 출력
-	//64비트 -> 포인터 8바이트
-	//32비트 -> 포인터 4바이트 
-	
-
-	//변수의 주소는 실행할 때마다 바뀐다.
-	//printf("data 주소: %p\n", &data);
-#pragma endregion
-
-#pragma region 포인터
-	//메모리의 주솟값을 저장할 수 있는 변수
-	int box = 100;
-	int box1 = 999;
-	int* ptr = &box;
-	//포인터 변수도 메모리 공간이 있고 변수의 주소를 저장하면 해당 변수의 시작 주소를 가리킨다
-	printf("ptr 값: %p\n", ptr);
-	printf("box 주소값 %p\n", &box);
-	printf("ptr 주소값 %p\n", &ptr);
-	printf("box 값: %d\n", box);
-	*ptr = 600;
-	printf("box 값: %d\n", box);
-	ptr = &box1;
-	*ptr = -999;
-	printf("box 값: %d\n", box);
-	printf("box1 값: %d\n", box1);
-	//변수의 자료형과 포인터 변수의 자료형이 일치해야 한다
-	double health = 10.5;
-	int* ptr1 = &health;
-	printf("ptr1 값: %p\n", ptr1);
-	*ptr1 = 51.5;
-	printf("ptr1가 가리키는 값: %lf\n", *ptr1);
-	
-#pragma endregion		
 }			
 			
