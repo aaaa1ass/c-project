2220b5c687db33eae1bf6739e65cdc78697c0f71
aaaa1ass <aaaa1ass@gmail.com>
2023-05-18 21:23:34 +0900

    2023/05/18
    동적 배열

---
+++ cproject/cproject/(2023.5.17)파일입출력.h
@@ -0,0 +1,59 @@
+#pragma once
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <limits.h>
+#include <malloc.h>
+#include <math.h>
+#include <string.h>
+#include <stdarg.h>
+#include <stdlib.h>
+#include <time.h>
+#include <conio.h>
+#include <windows.h>
+
+#define UP 72
+#define DOWN 80
+#define RIGHT 77
+#define LEFT 75
+
+int main()
+{
+#pragma region 파일 입출력
+	//파일 쓰기
+	//fopen("파일의 이름.확장자", 파일 모드)
+	// w: write
+	// r: read
+
+	//텍스트 파일 쓰기 모드로 열기
+	//FILE* filePtr = fopen("DB.txt", "w");
+	//
+	//fputs("ID\n", filePtr);
+	//fputs("Password\n", filePtr);
+	////파일 포인터 닫기
+	//fclose(filePtr);
+	int screen = 1;
+	//1 full screen
+	//2 window
+	FILE* readPtr = NULL;
+	while (1)
+	{
+		//SetConsoleDisplayMode(GetStdHandle(STD_OUTPUT_HANDLE), screen, 0);
+
+		//파일 읽기
+		readPtr = fopen("Monster.txt", "r");
+		char buffer[10000] = { 0, };
+		//파일 전체 읽기
+		fread(buffer, 1, 10000, readPtr);
+		printf("%s", buffer);
+		fclose(readPtr);
+		system("cls");
+		if (GetAsyncKeyState(VK_SPACE))
+		{
+			exit(0);
+		}
+	}
+	fclose(readPtr);
+#pragma endregion
+
+	return 0;
+}
--- cproject/cproject/ccc.c
+++ cproject/cproject/ccc.c
@@ -17,41 +17,55 @@
 
 int main()
 {
-#pragma region 파일 입출력
-	//파일 쓰기
-	//fopen("파일의 이름.확장자", 파일 모드)
-	// w: write
-	// r: read
-	
-	//텍스트 파일 쓰기 모드로 열기
-	//FILE* filePtr = fopen("DB.txt", "w");
+#pragma region 동적 배열
+	// heap을 이용하여 배열 크기를 실행 시간에 가변적으로 바꿀 수 있는 배열
 	//
-	//fputs("ID\n", filePtr);
-	//fputs("Password\n", filePtr);
-	////파일 포인터 닫기
-	//fclose(filePtr);
-	int screen = 1;
-	//1 full screen
-	//2 window
-	FILE* readPtr = NULL;
-	while (1) 
-	{
-		//SetConsoleDisplayMode(GetStdHandle(STD_OUTPUT_HANDLE), screen, 0);
+	//int size = 10;
+	//scanf("%d", &size);
+	//int * arrayPtr = (int*)malloc(sizeof(int)* size);
+	//for (int i = 0; i < size; i++)
+	//{
+	//	arrayPtr[i] = size * i;
+	//}
+	//for (int i = 0; i < size; i++)
+	//{
+	//	printf("arrayPtr[%d]: %d\n", i, arrayPtr[i]);
+	//}
+	//free(arrayPtr);
+#pragma endregion
 
-		//파일 읽기
-		readPtr = fopen("Monster.txt", "r");
-		char buffer[10000] = { 0, };
-		//파일 전체 읽기
-		fread(buffer, 1, 10000, readPtr);
-		printf("%s", buffer);
-		fclose(readPtr);
-		system("cls");
-		if (GetAsyncKeyState(VK_SPACE))
-		{
-			exit(0);
-		}
-	}
-	fclose(readPtr);
+#pragma region fflush() 함수
+	//char a, b, c;
+	//scanf("%c" ,&a);
+	//fflush(stdin);
+	//
+	//scanf("%c" ,&b);
+	//fflush(stdin);
+	//
+	//scanf("%c" ,&c);
+	//fflush(stdin);
+	//
+	//printf("a: %c\n", a);
+	//printf("b: %c\n", b);
+	//printf("c: %c\n", c);
+#pragma endregion
+
+#pragma region 문자열 뒤집기
+	//char string[100];
+	//scanf("%[^\n]s", string);
+	////char c;
+	////int count = 0;
+	////while (1)
+	////{
+	////	c = string[count];
+	////	if (c == NULL)
+	////		break;
+	////	count++;
+	////}
+	//for(int i = strlen(string) - 1; i >= 0; i--)
+	//{
+	//	printf("%c", string[i]);
+	//}
 #pragma endregion
 
 	return 0;
